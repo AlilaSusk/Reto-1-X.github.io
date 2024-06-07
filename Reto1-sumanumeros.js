5917fc94a816f799bb16e3508fe9a41ee3bd9a28
AlilaSusk <patylord7@gmail.com>
2024-06-06 21:57:53 -0500

    Cargue docs html y css suma
    html y css

---
+++ Reto1-sumanumeros.js
@@ -0,0 +1,9 @@
+//Programa suma de numeros y resultado
+
+var a= parseInt (prompt ("Ingrese un número: "));
+var b = prompt ("Ingrese otro numero: ");
+
+var resultado= a + parseInt(b);
+
+
+alert("El resultado es= " + resultado);
